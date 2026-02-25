937374290446adb8d255bf091683ffa52329f73c
Pilot12478 <igorzakurdaev6@gmail.com>
2026-02-25 19:59:26 +0400

    practice 4 fix

--- src/main/java/practice4/Tasks.java
+++ src/main/java/practice4/Tasks.java
@@ -1,5 +1,6 @@
 package practice4;
 
+
 import java.util.Scanner;
 
 public class Tasks {
@@ -21,22 +22,18 @@ public class Tasks {
     }
 
     //Поиск наибольшего из двух чисел
-    public static void ifElseTask2() {
-        Scanner scanner = new Scanner(System.in);
+    public static void ifElseTask2(Scanner scanner) {
         System.out.println("Введите первое число: ");
-        double num1 = scanner.nextDouble();
+        int num1 = scanner.nextInt();
         System.out.println("Введите второе число: ");
-        double num2 = scanner.nextDouble();
-        double max = Math.max(num1, num2);
-        if (max == (int) max) {
-            System.out.println("Наибольшее число: " + (int) max);
-        } else System.out.println("Наибольшее число: " + max);
+        int num2 = scanner.nextInt();
+        int max = Math.max(num1, num2);
+        System.out.println("Наибольшее число: " + max);
 
     }
 
     //Вывод оценки по шкале 1–5
-    public static void ifElseTask3() {
-        Scanner scanner = new Scanner(System.in);
+    public static void ifElseTask3(Scanner scanner) {
         System.out.println("Введите число: ");
         if (scanner.hasNextInt()) {
             int num = scanner.nextInt();
@@ -60,8 +57,7 @@ public class Tasks {
     }
 
     //Проверка на чётность
-    public static void ifElseTask4() {
-        Scanner scanner = new Scanner(System.in);
+    public static void ifElseTask4(Scanner scanner) {
         System.out.println("Введите число: ");
         int number = scanner.nextInt();
         if (number % 2 == 0) {
@@ -70,8 +66,7 @@ public class Tasks {
     }
 
     //Определение размера скидки по возрасту
-    public static void ifElseTask5() {
-        Scanner scanner = new Scanner(System.in);
+    public static void ifElseTask5(Scanner scanner) {
         System.out.println("Укажите Ваш возраст для расчета скидки: ");
         int age = scanner.nextInt();
         if (age < 18) {
@@ -82,8 +77,7 @@ public class Tasks {
     }
 
     //Оценка результата теста по баллам
-    public static void ifElseTask6() {
-        Scanner scanner = new Scanner(System.in);
+    public static void ifElseTask6(Scanner scanner) {
         System.out.println("Введите количество баллов: ");
         int point = scanner.nextInt();
         if (point >= 90) {
@@ -95,10 +89,9 @@ public class Tasks {
         } else System.out.println("неудовлетворительно");
 
     }
-    // Вывод дня недели по номеру
+// Вывод дня недели по номеру
 
-    public static void switchTask1() {
-        Scanner scanner = new Scanner(System.in);
+    public static void switchTask1(Scanner scanner) {
         System.out.println("Введите число: ");
         int day = scanner.nextInt();
         switch (day) {
@@ -130,8 +123,7 @@ public class Tasks {
     }
 
     //Стоимость билета по дню недели
-    public static void switchTask2() {
-        Scanner scanner = new Scanner(System.in);
+    public static void switchTask2(Scanner scanner) {
         System.out.println("Введите день недели в формате числа от 1 до 7: ");
         int day = scanner.nextInt();
         switch (day) {
@@ -147,8 +139,7 @@ public class Tasks {
     }
 
     //Перевод числовых оценок в буквенные (A–F)
-    public static void switchTask3() {
-        Scanner scanner = new Scanner(System.in);
+    public static void switchTask3(Scanner scanner) {
         System.out.println("Введите количество баллов");
         int num = scanner.nextInt();
         if (num >= 0 && num < 100) {
@@ -166,8 +157,7 @@ public class Tasks {
     }
 
     //Обработка текстовых команд
-    public static void switchTask4() {
-        Scanner scanner = new Scanner(System.in);
+    public static void switchTask4(Scanner scanner) {
         System.out.println("Введите команду: ");
         String command = scanner.nextLine();
         switch (command) {
@@ -187,8 +177,7 @@ public class Tasks {
     }
 
     //Простой калькулятор с использованием switch
-    public static void switchTask5() {
-        Scanner scanner = new Scanner(System.in);
+    public static void switchTask5(Scanner scanner) {
         System.out.println("Введите первое число: ");
         double num1 = scanner.nextDouble();
         System.out.println("Введите второе число: ");
@@ -230,21 +219,18 @@ public class Tasks {
     }
 
     //Сумма чисел от 1 до n
-    public static void forTask2() {
-        Scanner scanner = new Scanner(System.in);
+    public static void forTask2(Scanner scanner) {
         System.out.println("Введите число: ");
         int num = scanner.nextInt();
         int sum = 0;
         for (int i = 1; i <= num; i++) {
             sum = sum + i;
-            System.out.println(sum);
         }
         System.out.println(sum);
     }
 
     //Таблица умножения для числа
-    public static void forTask3() {
-        Scanner scanner = new Scanner(System.in);
+    public static void forTask3(Scanner scanner) {
         System.out.println("Введите число: ");
         int num = scanner.nextInt();
         for (int i = 0; i <= 10; i++) {
@@ -254,14 +240,19 @@ public class Tasks {
     }
 
     //Проверка на простое число
-    public static void forTask4() {
-        Scanner scanner = new Scanner(System.in);
+    public static void forTask4(Scanner scanner) {
         System.out.println("Введите число: ");
         int num = scanner.nextInt();
         boolean isPrime = true;
-        for (int i = 2; i <= num - 1; i++) {
-            if (num % i == 0) {
-                isPrime = false;
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= num - 1; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+
+                }
             }
         }
         if (isPrime == true) {
@@ -279,8 +270,7 @@ public class Tasks {
     }
 
     //Вычисление факториала с помощью while
-    public static void whileTask1() {
-        Scanner scanner = new Scanner(System.in);
+    public static void whileTask1(Scanner scanner) {
         System.out.println("Введите число:");
         int num = scanner.nextInt();
         int result = 1;
@@ -294,8 +284,7 @@ public class Tasks {
     }
 
     //Вывод всех чётных чисел до заданного
-    public static void whileTask2() {
-        Scanner scanner = new Scanner(System.in);
+    public static void whileTask2(Scanner scanner) {
         System.out.println("Введите число");
         int num = scanner.nextInt();
         int i = 1;
@@ -308,37 +297,33 @@ public class Tasks {
     }
 
     //Обратный отсчёт от введённого числа до 1
-    public static void whileTask3() {
-        Scanner scanner = new Scanner(System.in);
+    public static void whileTask3(Scanner scanner) {
         System.out.println("Введите число");
         int num = scanner.nextInt();
-        int i = 2;
         while (num > 0) {
             System.out.println(num);
             num--;
-            i++;
         }
     }
 
     //Запрос положительного числа
-    public static void doWhileTask1() {
-        Scanner scanner = new Scanner(System.in);
+    public static void doWhileTask1(Scanner scanner) {
         System.out.println("Введите положительно число");
         int num = scanner.nextInt();
         if (num >= 0) {
-
+            System.out.println("Введено положительное число");
         } else if (num < 0) {
             do {
                 System.out.println("Введите положительное число");
                 num = scanner.nextInt();
             } while (num < 0);
+            System.out.println("Введено положительное число");
         }
     }
 
     //проверка пароля
-    public static void doWhileTask2() {
+    public static void doWhileTask2(Scanner scanner) {
         String password = "DerParol";
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Введите пароль:");
         String userPass = scanner.nextLine();
         if (userPass.equals(password)) {
@@ -349,6 +334,7 @@ public class Tasks {
                 System.out.println("Введите пароль");
                 userPass = scanner.nextLine();
             } while (!userPass.equals(password));
+            System.out.println("Вы успешно вошли в систему");
         }
     }
 
@@ -362,11 +348,9 @@ public class Tasks {
     }
 
     //Завершение программы по команде "exit"
-    public static void doWhileTask4() {
+    public static void doWhileTask4(Scanner scanner) {
         String command = "exit";
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите команду");
-        String userCommand = scanner.nextLine();
+        String userCommand;
         int i = 0;
         do {
             System.out.println("Введите команду");
@@ -378,8 +362,7 @@ public class Tasks {
     }
 
     //Подсчёт количества цифр в числе
-    public static void doWhileTask5() {
-        Scanner scanner = new Scanner(System.in);
+    public static void doWhileTask5(Scanner scanner) {
         System.out.println("Введите число");
         int num = scanner.nextInt();
         int count = 0;
@@ -395,8 +378,7 @@ public class Tasks {
     }
 
     //Сумма чисел до первого отрицательного (использовать break)
-    public static void breakTask1() {
-        Scanner scanner = new Scanner(System.in);
+    public static void breakTask1(Scanner scanner) {
         System.out.println("Введите число: ");
         int num = scanner.nextInt();
         int i = 0;
@@ -423,43 +405,37 @@ public class Tasks {
     }
 
     //Вывод только положительных чисел (использовать continue)
-    public static void breakTask3() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число: ");
-        int num = scanner.nextInt();
-        if (num > 0) {
-            System.out.println(num);
-        }
-        int i = 0;
+    public static void breakTask3(Scanner scanner) {
+        int num;
         while (true) {
             System.out.println("Введите число: ");
             num = scanner.nextInt();
             if (num < 0) {
                 continue;
-            } else System.out.println(i);
-            i++;
+            } else if (num > 0) {
+                System.out.println(num);
+            } else break;
+
         }
     }
+
     //Ввод строк до команды "stop" (использовать break)
-    public static void breakTask4(){
+    public static void breakTask4(Scanner scanner) {
         String command = "stop";
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите команду");
-        String comm = scanner.nextLine();
-        int i =0;
-        while (true){
+        String comm;
+        while (true) {
             System.out.println("Введите команду: ");
             comm = scanner.nextLine();
-            if(comm.equals(command)){
+            if (comm.equals(command)) {
                 break;
             }
-            i++;
         }
 
     }
 
     static void main() {
-        Tasks.breakTask4();
+        Scanner scanner = new Scanner(System.in);
+        ifElseTask2(scanner);
 
     }
 
