3d92a7956f5f8bd46b991b848846967370c2a3f6
Pilot12478 <igorzakurdaev6@gmail.com>
2026-06-09 21:21:31 +0400

    practice 12

---
+++ src/main/java/practice11/DebugTask10.java
@@ -0,0 +1,16 @@
+package practice11;
+
+import java.util.*;
+public class DebugTask10 {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>(Arrays.asList("Alice", "Bob", "Charlie","All"));
+        Iterator<String> iterator = names.iterator();
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            if (name.startsWith("A")) {
+                iterator.remove(); // безопасное удаление
+            }
+        }
+    }
+}
+
---
+++ src/main/java/practice11/DebugTask8.java
@@ -0,0 +1,14 @@
+package practice11;
+
+public class DebugTask8 {
+    public static void main(String[] args) {
+        double a = 0.1 * 3;
+        double b = a;
+        if (a == b) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not Equal");
+        }
+    }
+}
+
